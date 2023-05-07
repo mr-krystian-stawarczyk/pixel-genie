b357497d819adceee275a2713e0dbb4e89e940d6
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-05-07 11:27:26 +0200

    zmiana dns

--- components/Header2.jsx
+++ components/Header2.jsx
@@ -16,9 +16,32 @@ function Header2() {
 				}}
 			>
 				<Col lg={6}>
-					<h1>Header 2</h1>
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
 				</Col>
-				<Col lg={6}></Col>
+				<Col lg={6}>
+					{" "}
+					<Card style={{ width: "18rem" }}>
+						<Card.Img variant="top" src="holder.js/100px180" />
+						<Card.Body>
+							<Card.Title>Card Title</Card.Title>
+							<Card.Text>
+								Some quick example text to build on the card title and make up
+								the bulk of the card's content.
+							</Card.Text>
+							<Button variant="primary">Go somewhere</Button>
+						</Card.Body>
+					</Card>
+				</Col>
 			</Row>
 		</Container>
 	);
