e41ac34acfa64e63ae5a90bfba967d85e7044d06
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-05-29 18:59:22 +0200

    eeerr6

--- pages/_app.js
+++ pages/_app.js
@@ -12,24 +12,6 @@ import { Partytown } from "@builder.io/partytown/react";
 function App(props) {
 	const { Component, pageProps, router } = props;
 
-	useEffect(() => {
-		if ("serviceWorker" in navigator) {
-			window.addEventListener("load", function () {
-				navigator.serviceWorker.register("/sw.js").then(
-					function (registration) {
-						console.log(
-							"ServiceWorker registration successful with scope: ",
-							registration.scope
-						);
-					},
-					function (err) {
-						console.log("ServiceWorker registration failed: ", err);
-					}
-				);
-			});
-		}
-	}, []);
-
 	return (
 		<SSRProvider>
 			<ThemeProvider defaultTheme="dark">
