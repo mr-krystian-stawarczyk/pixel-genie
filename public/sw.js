cb66917b9ca50c7b84501eae84c3c5a3206e719b
Krystian S <mr.krystian.stawarczyk@gmail.com>
2023-05-30 18:35:52 +0200

    default all

--- public/sw.js
+++ public/sw.js
@@ -4,10 +4,7 @@ const maxCacheSize = 5 * 1024 * 1024; // 5MB
 self.addEventListener("install", (event) => {
 	event.waitUntil(
 		caches.open(cacheName).then((cache) => {
-			return cache.addAll([
-				"/_next/server/pages/_app.js",
-				"/_next/static/chunks/main.js",
-			]);
+			return cache.addAll([]);
 		})
 	);
 });
